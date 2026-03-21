1f75712b0b7a5a380a386ec7c3fd0c3fc8b0c57f
pinfort <ptg@nijitei.com>
2026-03-21 17:27:10 +0900

    initialize

---
+++ backend/core/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.plugin.spring)
+	alias(libs.plugins.spring)
+	alias(libs.plugins.spring.dependency.management)
+}
+
+group = "me.pinfort"
+version = "0.0.1-SNAPSHOT"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(module = "mockito-core") // Kotestを使う
+	}
+	testImplementation(libs.kotest.runner.junit5)
+	testImplementation(libs.kotest.assertions.core)
+	testImplementation(libs.mockk)
+
+	// testcontainers
+	testImplementation(libs.testcontainers.core)
+	testImplementation(libs.testcontainers.junit5)
+	testImplementation(libs.testcontainers.mariadb)
+
+	testImplementation(libs.testcontainers.spring.boot)
+	testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+kotlin {
+	jvmToolchain(24)
+
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ backend/settings.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+rootProject.name = "ts-videos"
+
+include("core", "manager:infrastructure", "manager:console", "manager:api", "processor:infrastructure", "processor:console")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS) // プロジェクト側のrepositoriesを禁止
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
