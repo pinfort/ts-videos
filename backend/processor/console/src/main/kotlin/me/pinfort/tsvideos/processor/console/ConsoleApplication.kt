460cce932b5ea46f63425b94ec0c166cb49aaf81
pinfort <ptg@nijitei.com>
2026-08-24 22:19:55 +0900

    create application

---
+++ backend/processor/console/src/main/kotlin/me/pinfort/tsvideos/processor/console/ConsoleApplication.kt
@@ -0,0 +1,9 @@
+package me.pinfort.tsvideos.processor.console
+
+import org.springframework.boot.runApplication
+
+class ConsoleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConsoleApplication>(*args)
+}
